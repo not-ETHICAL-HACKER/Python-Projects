aba6a9368f9024912821372b7b8a33ac6c29feec
i-am-evil-hacker <johanwarrior333@gmail.com>
2025-11-04 17:26:46 +0530

    Update animation.py

--- idk/animation.py
+++ idk/animation.py
@@ -1,5 +1,5 @@
 import time,sys,random
-def marquee(txt:str="Hello World",dir:str="left",cd:float=0.1,num:int=10)->None:
+def marquee(txt:str=" Hello World ",dir:str="left",cd:float=0.1,num:int=10)->None:
     """"Creates a marquee animation in the console."""
     if dir not in ["left","right"]:
         raise ValueError("Direction must be 'left' or 'right'")
@@ -10,4 +10,20 @@ def marquee(txt:str="Hello World",dir:str="left",cd:float=0.1,num:int=10)->None:
             time.sleep(cd)
             txt=txt[1:]+txt[0]
     else:
-        pass
+        while True:
+            sys.stdout.write((txt)*num+"\r")
+            sys.stdout.flush()
+            time.sleep(cd)
+            txt=txt[-1]+txt[:-1]
+def bouncy(txt:str=" Hello World ",cd:float=0.1)->None:
+    """creates a magnet animation in the console."""
+    while True:
+        for i in range(len(txt)//2):
+            sys.stdout.write(" "*i+txt+"\r")
+            sys.stdout.flush()
+            time.sleep(cd)
+        for i in range(len(txt)//2,0,-1):
+            sys.stdout.write(" "*i+txt+"\r")
+            sys.stdout.flush()
+            time.sleep(cd)
+bouncy()
