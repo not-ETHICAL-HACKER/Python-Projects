13f532047d9971d74de37e08ce191c98b401146f
i-am-evil-hacker <johanwarrior333@gmail.com>
2025-11-21 17:26:58 +0530

    Update matrix_style_anim.py

--- new/matrix_style_anim.py
+++ new/matrix_style_anim.py
@@ -1,3 +1,25 @@
 def matrix(txt:str)->str:
-    pass
-    return ""
+    b=""
+    c=0
+    while c<len(txt):
+        for i in range(65, 91):
+            a=""+b
+            print(chr(i))
+            if chr(i)==txt[c]:
+                c+=1
+                b+=chr(i)
+    return a
+def matrix_2(txt: str) -> str:
+    b = ""
+    c = 0
+    while c < len(txt):
+        for i in range(65, 91):
+            print(chr(i))
+            if chr(i) == txt[c]:
+                b += chr(i)
+                c += 1
+                break           # stop scanning A–Z once matched
+    #! improve the idea of matrix the moviening letters
+    return b
+
+print(matrix_2("HELLO"))
