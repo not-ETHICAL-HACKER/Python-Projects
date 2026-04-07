62288b50378a6b2b4fcb0651e1aacf4da321d765
i-am-evil-hacker <johanwarrior333@gmail.com>
2026-04-07 09:44:20 +0530

    Update tribonacci_series.py

--- main/math/tribonacci_series.py
+++ main/math/tribonacci_series.py
@@ -3,10 +3,8 @@ def tribonnacci_series(i:int)->int:
     if i in (a,b):
         return a
     #f(n)=f(n-1)+f(n-2)+f(n-3)
-    k=1
-    while k<i:
+    for _ in range(3,i+1):
         print(c,end=" ")
         a,b,c=b,c,c+a+b
-        k+=1
     return c
 print(tribonnacci_series(10))
