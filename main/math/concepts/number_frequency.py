97f64945a3cd85b22397d31be09f473b48119510
i-am-evil-hacker <johanwarrior333@gmail.com>
2026-09-18 08:57:27 +0530

    Organize math modules; add number frequency script
    Refactor project layout by moving math modules into subpackages: approx files -> main/math/approx, concept-related modules (collatz_conjecture, factorial_vs_superfactorial, tower_of_lire, tribonacci_series) -> main/math/concepts, and simulations (equations, flow_fields, random_walk) -> main/math/math_sims. Add new script main/math/concepts/number_frequency.py which counts occurrences of a target substring in the range [0, n] (includes pacing via time.sleep).

---
+++ main/math/concepts/number_frequency.py
@@ -0,0 +1,14 @@
+import time
+n = 10**5
+s_num = "1"
+c = 0
+print("start")
+for i in range(n+1):
+    num = str(i)
+    if s_num in num:
+        # c += num.count(s_num)
+        c += 1
+        time.sleep(1/c)
+        print(f"num: {num:,} | Probability of {s_num} is {c/(n if n != 0 else 1)*100}%",end = "\r")
+print()
+print(f"'{s_num}' has appeared in [0,{n}] {c} times\nProbability of {s_num} is {c/(n if n != 0 else 1)*100}%")
